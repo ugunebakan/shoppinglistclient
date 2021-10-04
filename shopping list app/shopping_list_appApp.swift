7f09e321ddd932d3ea1bad7aac85fad89f66ebaf
Umut Günebakan <Umut.Gunebakan@netent.com>
2021-10-04 14:01:54 +0200

    Initial Commit

---
+++ shopping list app/shopping_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_list_appApp.swift
+//  shopping list app
+//
+//  Created by Umut Gunebakan on 2021-10-04.
+//
+
+import SwiftUI
+
+@main
+struct shopping_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
